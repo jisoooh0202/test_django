0fff4dcb9c858ef2060840e1e983671d06fb5d4a
jisoo-lenovo <jisoo@envisageworldwide.com>
2024-10-13 01:15:42 +0700

    add templates and static path to settings

--- test_django/settings.py
+++ test_django/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "test_django.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
