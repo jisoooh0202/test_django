3f010f732d70dd81a0de362b6e535228657093c2
jisoo-lenovo <jisoo@envisageworldwide.com>
2024-10-13 01:20:26 +0700

    setup models, urls, views for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django_ckeditor_5.fields import CKEditor5Field
+from simple_history.models import HistoricalRecords
+
 
 # Create your models here.
+class Blog(models.Model):
+    id: int
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(_("Title"), max_length=200)
+    content = CKEditor5Field(_("Content"), config_name="extends", blank=True)
+    author = models.CharField(
+        get_user_model(),
+        max_length=100,
+    )
+    history = HistoricalRecords(history_change_reason_field=models.TextField(null=True))
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Blog
+
 
 # Create your views here.
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog/blog_list.html"
+    context_object_name = "blogs"
